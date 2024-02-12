126489edd41ed461f15cc2d4236cd5ca08c8cdee
MishaBelyakin <miha.belyakin@yandex.ru>
2024-02-12 21:19:16 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,74 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello World 156456454646654564564565656564564!");
+        var dog = 8.0;
+        System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        dog = dog - 3.5;
+       System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
+        System.out.println(paper);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend =  friend + 2;
+        System.out.println(friend);
+        friend =  friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var boxerWeightRed = 78.2;
+        System.out.println("Масса красного боксера " + boxerWeightRed + " кг " );
+        var boxerWeightBlue = 82.7;
+        System.out.println("Масса синего боксера " + boxerWeightBlue + " кг " );
+        var totalWeight = boxerWeightRed + boxerWeightBlue ;
+        System.out.println("Общая масса боксеров " + totalWeight + " кг ");
+        var massDifference = boxerWeightBlue - boxerWeightRed;
+        System.out.println("Разница в весе между боксерами " + massDifference + " кг" );
+        var apples = 12;
+        System.out.println(apples);
+        var tomattos = 7;
+        System.out.println(tomattos);
+        var remainsofDivision = apples % tomattos ;
+        System.out.println("Остаток от деления = " + remainsofDivision );
+        var tWork = 640;
+        System.out.println("Общее время работы " + tWork + " часов");
+        var tMan = 8;
+        System.out.println("Время работы одного сотрудника " + tMan + " часов");
+        var man = tWork / tMan;
+        System.out.println("Всего работников в компании " + man + " человек");
+        man = man + 94;
+        System.out.println(man);
+        var tNew = tWork / man ;
+        System.out.println("Если в компании работает " + man + " человек, то всего " + tNew + " часов работы может быть поделено между сотрудниками" );
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
